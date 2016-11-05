e286f462ebc1897144e8798ca69ad448f68ab3fb
TeamLuebke <luebke@TeamLuebkes-MacBook-Pro.local>
2016-11-05 17:50:06 -0400

    Clarified Action functions further.

--- action.py
+++ action.py
@@ -13,13 +13,8 @@ class Action:
     (or None if the verb is intransitive), IDO is the indirect object (if any). 
     Action functions return True if the usage was valid, or an error message
     string if the usage was invalid (meaning nothing should happen except an 
-    error would be printed out). For example a sword object might support the
-    "swing sword" action, but only if the player is holding the sword. In this
-    case the action function would return True if the DO object was held by 
-    the player, otherwise returning 
-        'You can't swing the sword unless you are holding it!'
-    This allows the parser to do the right thing, for example, if the user 
-    "swing sword" when you are holding one sword while another is in the room.  
+    error would be printed out). The parser will print the appropriate error 
+    message if no valid handler is found.   
     """  
     def __init__(self, func=None, verbs=[], trans=False, intrans=False):
         self.func = func
