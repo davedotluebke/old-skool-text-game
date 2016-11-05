f0cf4469bccbc2e6fd79fce8dddd6e00d777ca32
Davy Luebke <dave@luebke.us>
2016-11-04 20:01:28 -0400

    Added adjectives to Plate.

--- OAD.TextGame.py
+++ OAD.TextGame.py
@@ -113,6 +113,7 @@ plate = Thing('plate')
 plate.set_description('a dinner plate', 'This is a normal-looking white dinner plate.')
 plate.set_weight(1000)
 plate.set_volume(1.25)
+plate.add_adjectives('dinner','white')
 kitchen.insert(plate)
 
 bird = NPC('bird', game)
