759bfdf57f87e03d846ad5395dc706e5a439943e
1Bayshore <owen@luebke.us>
2017-10-04 06:02:56 -0400

    Fixed error: Blue book in library not using gametools.clone() method

---
+++ domains/school/school/blue_book.py
@@ -0,0 +1,65 @@
+import book.Book as Book
+
+def clone():
+    blue_book = Book("blue book", None, "newer light blue book", "This book is newer, sky blue, and says \"Dragonsky\" on the cover.", pref_id="blue_book")
+    blue_book.add_names("book", "Dragonsky")
+    blue_book.add_adjectives("blue", "newer", "light")
+    blue_book.set_message('''
+
+    Dragonsky
+
+    by Owen Luebke
+    #*
+    Chapter 1
+        Once upon a time there were two twins. One was named Skylar and the other was named Jennifer. They were each other’s best friends too. They both enjoyed writing. Skylar wrote mostly poetry, and Jennifer wrote some non-fiction and a lot of autobiography, which was in a long book she wrote about her and Skylar’s lives. Her book stretched back to when she first learned to write at age 3, quite early. Their local elementary school was very supportive of their writing, and they were happy there. 
+        Skylar and Jennifer happened to live in Kenshalk, and it was a great fit for them. Jennifer wrote a lot about the historic buildings, and they both enjoyed the brightly colored streetcars. They even lived in one of the houses overlooking the expansive KTA museum and the tracks beyond. 
+        Skylar and Jennifer had many adventures. Occasionally they went camping, or they would visit places in town. They even went to other cities sometimes. They liked adventures almost anywhere. 
+        They had a treehouse up in one of the trees near the fence. They were not afraid of heights at all. In fact, they enjoyed feeling like they were flying from a high height. Sometimes sat up in the treehouse and wrote things. Skylar wrote many poems there, such as:
+            The wind rustles the leaves
+            In the spring
+            And it seems like fall
+
+            The sun glares down
+            In the spring
+            And it seems like summer
+
+            The bare trees sit
+            In the spring
+            And it seems like winter
+
+            Then the trees bloom
+            In the spring
+            And it seems like spring
+        They were preparing for their 12th birthday party. They were born in May, and so it happened as the school year let out. It was at
+    #*
+    Bounce Away!® because they liked to feel like they were flying, and the trampoline park did a pretty good job. Their entire class was invited, as well as some 4th graders and siblings of friends. It was a big group, and miraculously everyone could come. It was a really exciting event for everyone.
+        The day of the party was dreary and wet, but warm, so Skylar and Jennifer were happy they chose an indoor activity. Waiting at stoplight after stoplight on route 95 only made them more and more excited. When they arrived, they met everybody else quickly, and went in. The party had started.
+        They had a great time. They raced around, and they bounced in circles. And they tried to see who could jump the highest. Skylar and Jennifer were winning against everyone else. And they started jumping higher and higher. Then they both, at the same time, stuck out their arms and jumped even higher, and then started moving onto other trampolines. “Wee! We’re flying!” said Skylar. And then everyone started pretending to fly, and zipped around with their arms out. Skylar and Jennifer were bouncing so crazily that they had a hard time stopping before they went to their house for cake and ice cream.
+
+        That night, as they lay in their beds, they talked about the day.
+        “My favorite part was pretending to fly,” said Jennifer.
+        “I agree. We jumped really high.” said Skylar.
+        “We did. I was a good thing we didn’t hit the ceiling.”
+        “It reminded me of when we first jumped on a trampoline when we were 3.”
+        “Yeah. It was almost like we were actually flying.”
+        Skylar did not respond right away. His mind went to the trampoline at age 3, to an experience where he fell down a few stairs, and to today. When he replied, it was in a whisper. He said:
+        “I think maybe we can fly.”
+        “Maybe,” Jennifer whispered back, after a short pause.
+        “After another short pause, Skylar said, “I’m going to try.”
+        He got out of bed, and stood in the middle of the room. He just stood there for a second, then he moved his arms up and down. Slowly but surely, he rose off the ground. Jennifer just stared.
+        Then Jennifer got out of bed, and moved her arms up and down. She, too, went slowly but surely up into the air. They both looked at each other. 
+        “Come on, let’s fly to the tree house!” said Skylar. He and Jennifer shoved open the old window, trying to keep it from squeaking or slamming shut on them. Eventually, they forced it open, and wedged it open. They both stepped out onto the windowsill. 
+    #*
+        The warm rain drizzled down on them. It was not very strong anymore, just a little harder than mist. The ground was moist, visible from the second story window. It drizzled on.
+        Skylar and Jennifer both stood for a second on the windowsill, gazing out upon the woods, before they took off into the rainy night. Flying down to the treehouse, they twisted and turned and fell in crazy somersaults. They rocketed up and down, right and left, and broke a few small branches. They tried to aim for the treehouse, but it was no good. They went up above the treehouse. Skylar came to a stop. He stood there for a second and looked out.
+        Unfortunately, Jennifer’s landing was unexpected. She was a foot over the treehouse roof when she just dropped. “Ow!” she said quietly
+        “I’ve got one thing I want you to agree to,” said Skylar in a hushed voice.
+        “Is it ‘Don’t tell anyone’?” Jennifer whispered back.
+        “Yes!” replied Skylar.
+        They looked out over the KTA train museum and Banké Pacific tracks. No trains were running at the KTA museum, and one battered boxcar was sitting in the shadows on the Banké Pacific tracks. It said, “Banké Pacific Freight Express” and looked forlorn by itself.
+        An occasional car zipped past about once a minute on the road. The bright lights and sound of the wheels on the asphalt, as well as the new pavement and bright lines, made the road seem more active and important than the tracks and museum. The streetlights shone down brightly.
+        The warm rain trickled down, and the sound of dripping filled the air. It was light, but it was in a pattern almost, dripping as if it was part of a song. It was a nice sound, and dripped melodically.
+        Finally, Skylar and Jennifer departed back to their house. They went shooting up in the air as soon as they took off, and then tore straight for their windowsill. Skylar had to kick off the brick to stop himself. Then they slid into their room, jiggled their window down, trying to keep it from slamming or squeaking, and jumped back into their beds, where they lay wondering what they had discovered.
+    #*
+    ''')
+    return blue_book
