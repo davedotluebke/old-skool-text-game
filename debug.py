ba73ff44f8813b1c406f635f7eaaa76b39018e3e
David Luebke <dave@luebke.us>
2017-11-17 20:46:43 -0500

    Fixed debug() to be less verbose, and added ability to filter debug messages by a string.

--- debug.py
+++ debug.py
@@ -7,7 +7,8 @@ class DebugLog():
     Currently only supports printing and basic logging to a file."""
     def __init__(self, level=0):
         self.verbosity = int(level)  # current level of verbosity. 0 = no debug comments.     
-        self.set_logfile()     
+        self.filter_str = '&&&'  # a default value unlikely to appear in any code or debug messages
+        self.set_logfile()
 
     def set_logfile(self, filename="test_log.txt"):
         self.filename = filename
@@ -19,30 +20,36 @@ class DebugLog():
             print("Could not find a file at %s. Creating a new one.\n" % (self.filename))
             self.log = open(self.filename, 'c')
 
+    def set_filter_str(self, s):
+        self.filter_str = s
+
     def debug(self, s = "default error msg", level = 1):
-        """Print the string s if level is <= current verbosity level."""
-        if level > self.verbosity:
-            return
+        """Print the string s if level is <= current verbosity level, or if s includes a 
+        specified filter string. For example, if the filter string is set to 'Creature'
+        then debug() will print any debug strings containing 'Creature'. """
         s = str(s)
-        try:   #XXX temp fix
-            stack = inspect.stack()     # returns a list of FrameInfo tuples
-            class_name = stack[1].frame.f_locals["self"].__class__.__name__
-            func_name = class_name + '.' + stack[1].function + ":" + str(stack[1].lineno)
-            for f in stack[2:]:         
-                if f.function == "beat": break
-                try:
-                    caller_class = f.frame.f_locals["self"].__class__
-                    class_name = caller_class.__name__ + '.' if caller_class else ''
-                except KeyError:
-                    class_name = f.filename
-                func_name = class_name + f.function + ":" + func_name
-            
-            s = func_name + ": " + s
-        except KeyError:
-            s = "Error tracing stack, s = %s" % s
-        print(s)
-        if self.log:
-            self.log.write("%s\n" % s)
+        if level <= self.verbosity or self.filter_str in s:
+            try:   #XXX temp fix
+                stack = inspect.stack()     # returns a list of FrameInfo tuples
+                class_name = stack[1].frame.f_locals["self"].__class__.__name__
+                func_name = class_name + '.' + stack[1].function + ":" + str(stack[1].lineno)
+                for f in stack[2:]:         
+                    try:
+                        caller_class = f.frame.f_locals["self"].__class__
+                        class_name = caller_class.__name__ + '.' if caller_class else ''
+                    except KeyError:
+                        class_name = f.filename
+                    func_name = class_name + f.function + ":" + func_name
+                    if class_name == "Game.": break
+                
+                s = func_name + ": " + s
+            except KeyError:
+                s = "Error tracing stack, s = %s" % s
+            print(s)
+            if self.log:
+                self.log.write("%s\n" % s)
+        else:
+            return
 
     def shut_down(self):
         """The function that closes down the DebugLog class and file."""
--- parse.py
+++ parse.py
@@ -144,6 +144,15 @@ class Parser:
                 console.write(self._set_verbosity())
                 return True
             except ValueError:
+                if self.words[1] == 'filter':
+                    try:
+                        s = self.words[2:] 
+                        dbg.set_filter_str(s)
+                        console.write("Set verbose filter to '%s', debug strings containing '%s' will now be printed." % (s, s))                      
+                    except IndexError:
+                        dbg.set_filter_str('&&&')
+                        console.write("Turned off verbose filter; debug messages will only print if they are below level %d." % dbg.verbosity)
+                    return True
                 console.write("Usage: verbose [level]\n    Toggles debug message verbosity on and off (level 1 or 0), or sets it to the optionally provided <level>")
                 return True
             console.write(self._set_verbosity(level))
