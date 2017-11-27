150793fdd8fd62f43c42aaaa0032332dbc777e57
1Bayshore <owen@luebke.us>
2017-11-27 07:55:11 -0500

    Commiting the start of a web client. Does not fully work

--- test/websock.py
+++ test/websock.py
@@ -2,9 +2,14 @@ import asyncio
 import websockets
 
 async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+    try:
+        while True:
+            message = await websocket.recv()
+            await websocket.send(message)
+            print('sent message ' + message)
+    except websockets.exceptions.ConnectionClosed:
+        print('The connection was closed.')
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
+    websockets.serve(echo, 'localhost', 9124))
 asyncio.get_event_loop().run_forever()
