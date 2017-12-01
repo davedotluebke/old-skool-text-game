3250674bfd3f0539196017101c785f4d73b04df9
David Luebke <dave@luebke.us>
2017-11-30 20:05:13 -0500

    Added debug prints.

--- test/websock.py
+++ test/websock.py
@@ -3,8 +3,11 @@ import websockets
 
 async def echo(websocket, path):
     async for message in websocket:
+        print("received message '%s'" % message)
         await websocket.send(message)
+        print("Sent message '%s'" % message)
+        
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
+    websockets.serve(echo, '127.0.0.1', 9124))
 asyncio.get_event_loop().run_forever()
