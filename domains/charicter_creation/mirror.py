763a58de53260ab321101867cd6cddf5621af11b
1Bayshore <owen@luebke.us>
2017-12-02 05:48:29 -0500

    Added gender adding functionality to the Mirror class

--- domains/charicter_creation/mirror.py
+++ domains/charicter_creation/mirror.py
@@ -6,7 +6,13 @@ class Mirror(thing.Thing):
         self.set_description('shimmering mirror', 'In this mirror you see %s.' % what_you_see)
         self.actions.append(thing.Action(self.enter, ['enter'],True, False))
         self.dest = exit_room
+        self.add_race = None
+        self.add_gender = None
 
     def enter(self, p, cons, oDO, oIDO):
+        if self.add_race:
+            cons.user.race = self.add_race
+        if self.add_gender:
+            cons.user.gender = self.add_gender
         cons.user.move_to(self.dest)
         self.dest.report_arrival(cons.user)
--- domains/charicter_creation/start_loc.py
+++ domains/charicter_creation/start_loc.py
@@ -10,19 +10,23 @@ def load():
     r = room.Room('empty_room', roomPath)
     r.set_description('empty void', 'You find yourself in an empty void with a translucent floor. You see four mirrors, one north, one south, one east, and one west.')
 
-    human = mirror.Mirror('a human, smiling back at you', 'domains.charicter_creation.human')
+    human = mirror.Mirror('a human, smiling back at you', 'domains.charicter_creation.gender')
+    human.add_race = 'human'
     human.add_adjectives('north')
     r.insert(human)
 
-    elf = mirror.Mirror('an elf, smiling back at you', 'domains.charicter_creation.elf')
+    elf = mirror.Mirror('an elf, smiling back at you', 'domains.charicter_creation.gender')
+    elf.add_race = 'elf'
     elf.add_adjectives('south')
     r.insert(elf)
 
-    dwarf = mirror.Mirror('a dwarf, smiling back at you', 'domains.charicter_creation.dwarf')
+    dwarf = mirror.Mirror('a dwarf, smiling back at you', 'domains.charicter_creation.gender')
+    dwarf.add_race = 'dwarf'
     dwarf.add_adjectives('east')
     r.insert(dwarf)
 
-    gnome = mirror.Mirror('a gnome, smiling back at you', 'domains.charicter_creation.gnome')
+    gnome = mirror.Mirror('a gnome, smiling back at you', 'domains.charicter_creation.gender')
+    gnome.add_race = 'gnome'
     gnome.add_adjectives('west')
     r.insert(gnome)
     return r
