ceece27fd045c267dad66254b6ae6ff85cc2e35b
David Luebke <dave@luebke.us>
2017-12-22 21:03:25 -0500

    associate separate websocket connections with different Consoles
    Also moved connections_websockets.py out of webclient directory

--- connections_websock.py
+++ connections_websock.py
@@ -1,5 +1,7 @@
 import asyncio
 import websockets
+from thing import Thing
+from console import Console
 
 # From the websockets.serve() documentation:
 # Since there's no useful way to propagate exceptions triggered in handlers,
@@ -10,13 +12,24 @@ logger = logging.getLogger('websockets.server')
 logger.setLevel(logging.ERROR)
 logger.addHandler(logging.StreamHandler())   
 
+clients = dict()  # dictionary mapping client consoles to their websockets
+
 # The callback that does everything in the webserver:
 async def handler_callback(websocket, path):
+    # figure out which client this handler corresponds to
+    global clients  
+    try: 
+        client_cons = clients[websocket]
+    except KeyError:
+        print("websocket not associated with a client, making new client!")
+        client_cons = Console(websocket, Thing.game)
+        clients[websocket] = client_cons
+
     async for message in websocket:
-        print("received message '%s'" % message)
+        print("received message '%s' from client %s" % (message, client_cons))
         message = "I, the server, reply: " + message
         await websocket.send(message)
-        websocket.
+        
         print("Sent message '%s'" % message)
 
 
