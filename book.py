aee38bad14bd986327e9498493b884fd38e5e2f3
Daniel Huss <dhuss83@gmail.com>
2017-12-28 19:49:20 -0600

    Fixed a few bugs and added more emits
    Fixes #47
    The characters " \t\r\n" are all stripped before checking for a page break.
    Fixed a bug when each successive attempt to read after reaching the end of a book would spawn a message like "can't find page 6", and then "can't find page 7", etc.
    Fixed an emit that threw an error every time because it referenced self.s_desc instead of self.short_desc.
    Added a few more writes and emits.
    Fixed spacing in book.py and removed an unnecessary page break at the end which created a blank page.

--- book.py
+++ book.py
@@ -12,28 +12,25 @@ class Book(Thing):
         self.add_names('book')
 
     def set_message(self, message):
-        page_num = 0
-        c = 0
-        page = '\n'
-        for i in message.splitlines(True):
-            if i == "#*\n":
-                page_num += 1
-                c = 0
-                try:
-                    self.what_you_read[page_num] = page
-                except IndexError:
-                    self.what_you_read.append(page)
-                page = '\n'
+        self.what_you_read = list()
+        page_text = '\n'
+
+        for line_text in message.splitlines(True):
+            if line_text.strip(' \t\r\n') == "#*": 
+                ## page break ##
+                self.what_you_read.append(page_text)
+                page_text = '\n'
                 continue
-            page += i
-            c += 1
-            # #* means page break
-        try:
-            self.what_you_read[page_num] = (page + '\n\n')
-        except IndexError:
-            self.what_you_read.append(page + '\n\n')
+            page_text += line_text 
+
+        self.what_you_read.append(page_text + '\n\n')
 
     def read(self, p, cons, oDO, oIDO):
+        '''
+            case == 1: read current page
+            case == 2: read specific page
+            case == 3: read next page
+        '''
         if self not in cons.user.contents:
             cons.write('You need to take the book before reading it!')
             return True
@@ -44,40 +41,64 @@ class Book(Thing):
                 case = 3
             else:
                 case = 1
-        # see if they typed "read page #" 
-        match = re.search(r'page (\d+)', sDO)
-        if match:
-            pagenum = match.group(1)
-            case = 2
+
         if not case:
-            match = re.search(r'next page', sDO)
+            # search the direct object string (sDO) to determine which page to read
+            match = re.search(r'page (\d+)', sDO)
             if match:
+                #> read page #
+                pagenum = match.group(1)
+                case = 2
+            elif re.search(r'next page', sDO):
+                #> read next page
                 case = 3
-            if not case:
-                match = re.search(r'page', sDO)
-                if match:
-                    case = 1
-                if not case:
-                    match = re.search(r'(\S+)', sDO)
-                    if match:
-                        if match.group(1) in self.names:
-                            case = 1
-        try:
-            if self.what_you_read and (case == 1):
-                cons.write("You read:"+str(self.what_you_read[self.index]), 8)
-                cons.user.reading = True
-            elif self.what_you_read and (case == 2):
-                self.index = int(pagenum)-1
-                cons.write('You flip to page '+str(self.index+1)+'.')
-                cons.write("You read:"+str(self.what_you_read[self.index]), 8)
-                cons.user.reading = True
-            elif self.what_you_read and (case == 3):
-                self.index += 1
-                cons.write("You read:"+str(self.what_you_read[self.index]), 8)
-                cons.user.reading = True
+            elif re.search(r'page', sDO):
+                #> read page
+                case = 1
             else:
-                cons.write("A problem occured!")
-            cons.user.emit("&nD%s reads from the %s." %(cons.user.id, self.s_desc))
+                match = re.search(r'(\S+)', sDO)
+
+                if match and match.group(1) in self.names:
+                    #> read [this book's name]
+                    case = 1
+        try:
+            can_read = True
+
+            # select the appropriate page to read
+            if not self.what_you_read:
+                # there's nothing to read
+                can_read = False
+                cons.write("It appears to be blank...")
+                cons.user.emit("&nD%s stares confusedly at the %s." %(cons.user.id, self.short_desc))
+            elif (case == 2):
+                if 0 < int(pagenum) <= len(self.what_you_read):
+                    if (self.index != int(pagenum)-1):
+                        # not the same page as last time
+                        self.index = int(pagenum)-1
+                        cons.write('You flip to page ' + str(self.index+1) + '.')
+                else:
+                    raise IndexError
+            elif (case == 3):
+                if (self.index+1 == len(self.what_you_read)):
+                    can_read = False
+                else: 
+                    self.index += 1
+
+            if can_read:
+                cons.write("You read:" + str(self.what_you_read[self.index]), 8)
+                if not cons.user.reading:
+                    cons.user.emit("&nD%s opens the %s and starts to read." %(cons.user.id, self.short_desc))
+                else:
+                    cons.user.emit("&nD%s flips through the pages of the %s thoughtfully." %(cons.user.id, self.short_desc))
+                cons.user.reading = True
+            elif not self.what_you_read:
+                # blank book
+                cons.write("It appears to be blank...")
+                cons.user.emit("&nD%s stares confusedly at the %s." %(cons.user.id, self.short_desc))
+            else:
+                # end of book
+                cons.write("There's nothing left to read.")
+
         except IndexError:
-            cons.write('The book does not have a page numbered %s!' % str(self.index+1))
+            cons.write("You can't seem to find that page...")
         return True
