b4313594245974df3b60c8d6d2735f4e3d7bc4bd
1Bayshore <owen@luebke.us>
2018-05-17 05:29:37 -0400

    Added attack_verbs to sword and hammer

--- domains/school/forest/sword.py
+++ domains/school/forest/sword.py
@@ -1,7 +1,7 @@
 import weapon
 
 def clone():
-    sword = weapon.Weapon('sword', __file__, 6, 30, 2)
+    sword = weapon.Weapon('sword', __file__, 6, 30, 2, attack_verbs=["swing","stab","thrust"])
     sword.set_description('rusty old sword', "This is a rusty old sword. It's not very sharp but is still better than bare hands.")
 
     return sword
