3b220ffd4a860be9cf9f5e78327e48e199d038c7
1Bayshore <owen@luebke.us>
2018-06-12 06:22:24 -0400

    Began updates to scroll Still many problems

--- domains/school/scroll.py
+++ domains/school/scroll.py
@@ -7,19 +7,26 @@ class Scroll(Thing):
         super().__init__(default_name, path, pref_id)
         self.set_description('scroll', 'This scroll appears to say something on it.')
         self.actions.append(Action(self.read, ['read'], True, False))
-        self.messages = ['Make a potion to hide thee. Use this potion to sneak past an unbeatable enemy.\nDue next class.', 'Thou must continue on thy journey\nAnd in thy journey\nThou must choose a path\nAnd thou must be careful, because thy choice defines thy destiny. ', 'Now that thou hast found thy power, thou must set off into the world.\nExplore the lands of Sorcery and Wizardry, Enchantment and Witchcraft. A letter will arrive for thee awaiting thy return.\n\nWrite the true name of a place on the magic paper, and go west to travel there. Write "learn wizardry", and go west to learn wizardry; or write "learn witchcraft" to learn witchcraft, "learn sorcery" to learn sorcery or "learn enchantment" to learn enchantment.']
+        self.messages = ['Make a potion to hide thee. Use this potion to sneak past an unbeatable enemy.\nDue next class.', 
+        'Thou must continue on thy journey\nAnd in thy journey\nThou must choose a path\nAnd thou must be careful, because thy choice defines thy destiny. ',
+        'Thy class begins shortly.']
         self.message_number = 0
-        self.triggers = ['domains.school.dungeon.dungeon_hall','domains.school.elementQuest.waterfall','EOFError no more Scroll messages;']
-        self.next_trigger_room = 'scrollchangeroom1'
+        self.triggers = ['domains.school.dungeon.dungeon_hall', 'domains.school.school.great_hall']
+        self.next_trigger_room = self.triggers[0]
         self.glow_when_enter_rooms = [['domains.school.elementQuest.path_choice', False]]
+        Thing.game.register_heartbeat(self)
 
     def heartbeat(self):
-        if self.location.id == self.next_trigger_room:
+        r = self
+        while r.location:
+            r = r.location
+        
+        if r.id == self.next_trigger_room:
             self.emit('The scroll glows for a second!')
             self.message_number += 1
             self.next_trigger_room = self.triggers[self.message_number]
         for i in self.glow_when_enter_rooms:
-            if i[1] == self.location:
+            if i[1] == r.id:
                 self.emit('The scroll glows for a second!')
                 i[2] == True
 
