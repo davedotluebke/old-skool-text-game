3a99fda9254ff6dd58166e9b3b710b24dbff1ffe
1Bayshore <owen@luebke.us>
2018-06-13 05:53:25 -0400

    Simplified and fixed the scroll

--- domains/school/scroll.py
+++ domains/school/scroll.py
@@ -7,13 +7,16 @@ class Scroll(Thing):
         super().__init__(default_name, path, pref_id)
         self.set_description('scroll', 'This scroll appears to say something on it.')
         self.actions.append(Action(self.read, ['read'], True, False))
-        self.messages = ['Make a potion to hide thee. Use this potion to sneak past an unbeatable enemy.\nDue next class.', 
-        'Thou must continue on thy journey\nAnd in thy journey\nThou must choose a path\nAnd thou must be careful, because thy choice defines thy destiny. ',
-        'Thy class begins shortly.']
-        self.message_number = 0
-        self.triggers = ['domains.school.dungeon.dungeon_hall', 'domains.school.school.great_hall']
-        self.next_trigger_room = self.triggers[0]
-        self.glow_when_enter_rooms = [['domains.school.elementQuest.path_choice', False]]
+        self.current_message = ''
+        self.messages = {
+            'domains.school.school.great_hall':        ['Make a potion to hide thee. Use this potion to sneak past an unbeatable enemy.\nDue next class.', False],
+            'domains.school.dungeon.dungeon_hall':     ['Thou must continue on thy journey\nAnd in thy journey\nThou must choose a path\nAnd thou must be careful, because thy choice defines thy destiny. ', False],
+            'domains.school.elementQuest.path_choice': ['Thou must continue on thy journey\nAnd in thy journey\nThou must choose a path\nAnd thou must be careful, because thy choice defines thy destiny. ', False],
+            'domains.school.school.fire_lounge':       ['Thy class begins shortly.', False],
+            'domains.school.school.water_lounge':      ['Thy class begins shortly.', False],
+            'domains.school.school.earth_lounge':      ['Thy class begins shortly.', False],
+            'domains.school.school.air_lounge':        ['Thy class begins shortly.', False]
+        }
         Thing.game.register_heartbeat(self)
 
     def heartbeat(self):
@@ -21,20 +24,19 @@ class Scroll(Thing):
         while r.location:
             r = r.location
         
-        if r.id == self.next_trigger_room:
-            self.emit('The scroll glows for a second!')
-            self.message_number += 1
-            self.next_trigger_room = self.triggers[self.message_number]
-        for i in self.glow_when_enter_rooms:
-            if i[1] == r.id:
+        if r.id in list(self.messages):
+            if self.messages[r.id][1] == True:
+                return
+            else:
                 self.emit('The scroll glows for a second!')
-                i[2] == True
+                self.current_message = self.messages[r.id][0]
+                self.messages[r.id][1] = True
 
     def read(self, p, cons, oDO, oIDO):
         if oDO != self:
             return "Are you intending to read the scroll?"
         if len(p.words) == 2:
-            cons.write('On the scroll there are these words:\n'+self.messages[self.message_number])
+            cons.write('On the scroll there are these words:\n'+self.current_message)
             return True
         else:
             return "A problem occurred!"
