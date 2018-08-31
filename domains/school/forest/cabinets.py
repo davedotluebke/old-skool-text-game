59fd8e8e48e0bda051107f14beff7f962913ab12
1Bayshore <owen@luebke.us>
2018-08-30 20:34:22 -0500

    Made cabinets pluralities

--- domains/school/forest/cabinets.py
+++ domains/school/forest/cabinets.py
@@ -2,14 +2,13 @@ import container
 import gametools
 
 def clone():
-    cabinets = container.Container('cabinets', __file__)
-    cabinets.set_description('bunch of cabinets', 'The lightly stained wooden cabinets in this kitchen are slightly dusty.')
-    cabinets.fix_in_place("How do you think you can take cabinets!? You can\'t.")
-    cabinets.add_names('cabinet')
+    cabinets = container.Container('cabinet', __file__)
+    cabinets.set_description('lightly stained wood cabinet', 'This lightly stained wooden cabinet is slightly dusty.')
+    cabinets.fix_in_place("You cannot take cabinets.")
     cabinets.add_adjectives('wood', 'lightly stained','stained','old','1960s',"1960's",'60s',"60's")
     cabinets.set_max_volume_carried(5000)
     cabinets.set_max_weight_carried(100000)
-    cabinets.plural = True
+    cabinets.plurality = 3
     cabinets.closable = True
     cabinets.close()
 
