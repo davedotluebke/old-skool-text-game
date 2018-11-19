cbd3361dfd7218c96e5dc2e92885516a6dcf323e
1Bayshore <owen@luebke.us>
2018-11-19 05:53:56 -0500

    Fixed a few bugs

--- domains/centrata/firefile_area/cave/small_tunnel.py
+++ domains/centrata/firefile_area/cave/small_tunnel.py
@@ -10,6 +10,6 @@ def load():
     r = room.Room('tunnel', roomPath, light=0, indoor=True)
     r.set_description('small tunnel', 'This small tunnel looks as if it was carved carefully from the rock. You hear the sound of rushing water to the south.')
     r.add_exit('south', 'domains.school.forest.underground_waterfall')
-    r.add_exit('north', 'domains.centrata.firefile_areas.cave.carved_staircase')
+    r.add_exit('north', 'domains.centrata.firefile_area.cave.carved_staircase')
 
     return r
