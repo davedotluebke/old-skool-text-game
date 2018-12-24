798ce7dc70743f22fe3680153522a9591c31a956
1Bayshore <owen@luebke.us>
2018-12-23 20:03:57 -0500

    Added preliminary scenery changing to kings road

--- domains/centrata/kings_road.py
+++ domains/centrata/kings_road.py
@@ -2,6 +2,7 @@
 import gametools
 import scenery
 import room
+import random
 
 def load(param_list):
     path = param_list[0] # if paramaters are given, the first one is always the entire string, including parameters
@@ -11,7 +12,16 @@ def load(param_list):
     road_section = int(param_list[1])
 
     this_road = room.Room('road', path)
-    this_road.set_description('streach of road', 'This stretch of road (section %s) continues through the fields, north and south.' % road_section)
+
+    random.seed(road_section)
+
+    nouns = ['mountains', 'forests', 'woods', 'hills', 'plains', 'praries', 'grasslands', 'swamps', 'lakes']
+    adjectives = ['woody', 'gorgeous', 'historic', 'dense', 'open', 'tall', 'short', 'deep', 'frozen']
+
+    west_side = 'some %s %s' % (random.choice(adjectives), random.choice(nouns))
+    east_side = 'some %s %s' % (random.choice(adjectives), random.choice(nouns))
+
+    this_road.set_description('streach of road', 'This stretch of road (section %s) continues north and south. On the east side of the road, you see %s. On the west side of the road, you see %s.' % (road_section, west_side, east_side))
 
     this_road.add_exit('north', 'domains.centrata.kings_road?%s' % str(road_section+1))
 
