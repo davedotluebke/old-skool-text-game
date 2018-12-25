4464873bb8eda691018fbd940f707f4d7cff7dc4
1Bayshore <owen@luebke.us>
2018-12-25 06:42:12 -0500

    Enhanced descriptions and added ends to the road

--- domains/centrata/kings_road.py
+++ domains/centrata/kings_road.py
@@ -9,25 +9,40 @@ def load(param_list):
     exists = room.check_loaded(path)
     if exists: return exists
     
-    road_section = int(param_list[1])
+    road_start = param_list[1]
+    road_end = param_list[2]
+    road_length = int(param_list[3])
+    road_section = int(param_list[4])
 
     this_road = room.Room('road', path)
 
-    random.seed(road_section)
+    random.seed(road_section*road_length)
 
-    nouns = ['mountains', 'forests', 'woods', 'hills', 'plains', 'praries', 'grasslands', 'swamps', 'lakes']
-    adjectives = ['woody', 'gorgeous', 'historic', 'dense', 'open', 'tall', 'short', 'deep', 'frozen']
+    description_map = {'mountains':['tall', 'steep', 'magnificent', 'large', 'distant'],
+                       'forests':  ['pine','evergreen', 'thick', 'dense', 'sparse', 'dead'],
+                       'woods':    ['light', 'open', 'thin', 'thick'],
+                       'hills':    ['small', 'larger', 'wooded', 'grassy'],
+                       'plains':   ['wide', 'huge', 'empty'],
+                       'praries':  ['tall', 'brown-grassed', 'dry'],
+                       'swamps':   ['deep', 'wooden', 'wet', 'huge', 'small'],
+                       'lakes':    ['deep', 'frozen', 'huge', 'small']}
 
-    west_side = 'some %s %s' % (random.choice(adjectives), random.choice(nouns))
-    east_side = 'some %s %s' % (random.choice(adjectives), random.choice(nouns))
+    noun1 = random.choice(list(description_map))
+    noun2 = random.choice(list(description_map))
+
+    west_side = 'some %s %s' % (random.choice(description_map[noun1]), noun1)
+    east_side = 'some %s %s' % (random.choice(description_map[noun2]), noun2)
 
     this_road.set_description('streach of road', 'This stretch of road (section %s) continues north and south. On the east side of the road, you see %s. On the west side of the road, you see %s.' % (road_section, west_side, east_side))
 
-    this_road.add_exit('north', 'domains.centrata.kings_road?%s' % str(road_section+1))
-
-    if road_section > 0:
-        this_road.add_exit('south', 'domains.centrata.kings_road?%s' % str(road_section-1))
+    if road_section == 0:
+        this_road.add_exit('south', road_start)
     else:
-        this_road.add_exit('south', 'domains.centrata.firefile_area.fields.road_four')
+        this_road.add_exit('south', 'domains.centrata.kings_road?%s&%s&%s&%s' % (road_start, road_end, road_length, str(road_section-1)))
+
+    if road_section == road_length - 1:
+        this_road.add_exit('north', road_end)
+    else:
+        this_road.add_exit('north', 'domains.centrata.kings_road?%s&%s&%s&%s' % (road_start, road_end, road_length, str(road_section+1)))
 
     return this_road
