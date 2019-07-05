ffb161189436137f7b89b921ac447d52961bd7e1
1Bayshore <owen@luebke.us>
2019-07-05 10:34:23 -0400

    Made a few minor changes

--- home/scott/house/btr31795.py
+++ home/scott/house/btr31795.py
@@ -9,20 +9,20 @@ def load():
     
     bathroom = room.Room('bathroom', pref_id=roomPath, indoor=True)
     bathroom.set_description('modern bathroom', 'This small bathroom has a bathtub, a shower, and a sink.')
-    bathroom.add_exit('east', 'home.alex.house.lr31795')
+    bathroom.add_exit('east', 'home.scott.house.lr31795')
     bathroom.add_adjectives('modern')
 
-    bathtub = gametools.clone('home.alex.house.bathtub')
+    bathtub = gametools.clone('home.scott.house.bathtub')
     bathtub.move_to(bathroom, True)
 
-    sink = gametools.clone('home.alex.house.sink')
+    sink = gametools.clone('home.scott.house.sink')
     sink.move_to(bathroom, True)
 
     toilet = scenery.Scenery('toilet', 'ordinary toilet', 'This is an ordinary toilet.')
     toilet.add_response(['flush'], 'You flush the toilet.')
     toilet.move_to(bathroom, True)
 
-    cabinet = gametools.clone('home.alex.house.cabinets')
+    cabinet = gametools.clone('home.scott.house.cabinets')
     cabinet.move_to(bathroom, True)
 
     return bathroom
