73f6aa6e3330695912fd7a971932be14cad5a5bc
1Bayshore <owen@luebke.us>
2019-07-28 15:13:41 -0400

    Added an arena

--- domains/school/school/classroom_hallway.py
+++ domains/school/school/classroom_hallway.py
@@ -10,4 +10,5 @@ def load():
     hallway.set_description('long hallway', 'This is a long hallway with many doors off of it. The one to the south catches your eye.')
     hallway.add_exit('west', 'domains.school.school.towerstairs')
     hallway.add_exit('south', 'domains.school.school.classroom1')
+    hallway.add_exit('east', 'domains.school.school.arena')
     return hallway
