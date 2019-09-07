d1d12f1bd2da9333d1672709c94207e90b0d38f9
1Bayshore <owen@luebke.us>
2019-09-07 14:35:51 -0400

    Added daily git commits

--- startup.sh
+++ startup.sh
@@ -4,15 +4,5 @@ while :
 do
     git add -A
     git commit -m "Daily commit to add wizard-generated files to GitHub."
-    git pull
-    FAIL=$?
-    if ((FAIL))
-    then
-        git reset --hard
-        echo "Merge conflict occured."
-    else
-        git push
-        echo "No merge conflicts."
-    fi
     python3 startup.py
 done
