a8e8beb3dfa2403de8a64ba2ecc6e69435c17f00
1Bayshore <owen@luebke.us>
2019-09-08 07:00:56 -0400

    scott uploaded file test.py

--- home/scott/examples/test.py
+++ home/scott/examples/test.py
@@ -1,20 +1,27 @@
-import gametools
-import scenery
 import room
+import gametools
+import home.scott.house.exit_door as exit_door
 
 def load():
-    roomPath =  gametools.findGamePath(__file__)
+    roomPath = gametools.findGamePath(__file__)
     exists = room.check_loaded(roomPath)
     if exists: return exists
     
-    maze_entrance = room.Room('small cavern', roomPath, indoor=True)
-    maze_entrance.set_description('small cavern','This is a small cavern. To the the east and west two winding passageways lead off into the darkness.')
-    maze_entrance.add_exit('east', 'domains.centrata.firefile_area.key_quest.maze28')
-    maze_entrance.add_exit('west', 'domains.centrata.firefile_area.key_quest.maze41')
+    bedroom = room.Room('bedroom', pref_id=roomPath, indoor=True, safe=True)
+    bedroom.set_description('circular bedroom', 'This small circular bedroom is built in the top of a tower. '
+    'It has four large windows: one to the north, one to the south, one to the east, and one to the west.'
+    'Strangely, each of the windows appears to look out on extremely different locations.')
+    bedroom.add_exit('down', 'home.scott.house.lr31795')
+    bedroom.add_adjectives('circular', 'windowed')
 
-    ruby = gametools.clone('home.scott.house.ruby')
-    maze_entrance.insert(ruby)
+    north_window = exit_door.Window('window', 'northern window', 'This large window on the north side of the room has an almost invisible frame.', 'domains.school.forest.waterfall')
+    north_window.add_adjectives('large', 'north')
+    bedroom.insert(north_window, True)
 
-    emerald = gametools.clone('home.scott.house.emerald')
-    maze_entrance.insert(emerald)
-    return maze_entrance
+    south_window = exit_door.Window('window', 'southern window', 'This large window on the south side of the room has an almost invisible frame.', 'domains.school.forest.waterfall')
+    south_window.add_adjectives('large', 'south')
+    bedroom.insert(south_window, True)
+
+    bed = gametools.clone('home.scott.house.bed')
+    bed.move_to(bedroom, True)
+    return bedroom
