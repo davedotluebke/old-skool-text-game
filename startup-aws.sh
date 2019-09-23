012547c54c3f2abf6d332f68784b376640840011
EC2 Default User <ec2-user@ip-172-31-88-221.ec2.internal>
2019-09-23 04:58:03 +0000

    Daily commit to add wizard-generated files to GitHub.

--- startup-aws.sh
+++ startup-aws.sh
@@ -4,7 +4,7 @@ KEEP_GOING=0
 until [ $KEEP_GOING -gt 0 ]
 do
 
-    myAddress="$(curl -s http://169.254.169.254/latest/meta-data/hostname)"  # magic command for finding public IP of current instance
+    myAddress="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"  # magic command for finding public IP of current instance
     python3 startup.py --server "$myAddress"
     KEEP_GOING=$?
     git add -A
