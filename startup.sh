d7a96d5a639b84335beb94403891bb672ff0fdfd
davedotluebke <dave@luebke.us>
2019-09-23 00:02:38 -0400

    Daily commit to add wizard-generated files to GitHub.

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ until [ $KEEP_GOING -gt 0 ]
 do
     git add -A
     git commit -m "Daily commit to add wizard-generated files to GitHub."
-    python3 startup.py
+    python3 startup.py --server 127.0.0.1
     KEEP_GOING=$?
 done
