7921fcdc857489bf52cf410e8e47e719a0e359c5
EC2 Default User <ec2-user@ip-172-31-88-221.ec2.internal>
2019-10-01 10:16:47 +0000

    Daily commit to add wizard-generated files to GitHub.

--- connections_websock.py
+++ connections_websock.py
@@ -77,5 +77,5 @@ async def file_send(cons):
         if isinstance(output[i], bytes):
             output[i] = output[i].decode('utf-8')
     cons.file_output = bytes()
-    print(output)
-    await cons.connection.send(output)
+    final_output = json.dumps(output)
+    await cons.connection.send(final_output)
