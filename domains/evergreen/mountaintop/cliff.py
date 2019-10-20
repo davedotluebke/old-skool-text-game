5f4fe4a9d4bf379df35fefe92793c88ffe50a057
1Bayshore <owen@luebke.us>
2019-10-20 08:48:11 -0400

    Began working on a new domain

---
+++ domains/evergreen/mountaintop/cliff.py
@@ -0,0 +1,17 @@
+import gametools
+import room
+import scenery
+from domains.school.school.hallway import StaffDoorway
+
+def load():
+    roomPath = gametools.findGamePath(__file__)
+    exists = room.check_loaded(roomPath)
+    if exists: return exists
+    
+    cliff = room.Room('cliff', pref_id=roomPath)
+    cliff.set_description('clifftop', 'You walk out onto a massive clifftop, overlooking an endless evergreen forest carpeted with snow. You see a tiny shack with a pine door.')
+
+    pine_door = StaffDoorway('door', 'pine door', 'This is a strong pine door.', 'home.scott.house.er31795', allowed_players_list=['scott', 'tate', 'cedric', 'rivques'])
+    pine_door.add_adjectives('pine', 'strong')
+    pine_door.move_to(cliff, True)
+    return cliff
--- domains/school/school/dragon_scale.py
+++ domains/school/school/dragon_scale.py
@@ -5,4 +5,5 @@ def clone():
     scale.set_description('golden dragon scale', 'This is a golden dragon scale. It is used in many very strong potions.')
     scale.add_adjectives('dragon', 'golden')
     scale.add_names('scale')
+    scale.set_value(70)
     return scale
