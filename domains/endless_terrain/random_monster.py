eb6499af343bd8c9c9e0e0eba391e5847bac614a
1Bayshore <owen@luebke.us>
2019-11-16 20:12:22 -0500

    Updated random monsters

--- domains/endless_terrain/random_monster.py
+++ domains/endless_terrain/random_monster.py
@@ -1,17 +1,24 @@
 # NPC factory: This module creates a random creature based on a coordinate pair, rather than a specific one
 import random
 import creature
+from collections import namedtuple
+
+Monster_stats = namedtuple('MonsterData', ['name', 'plural_name', 'probability', 'av_ac', 'av_combat', 'av_strength', 'av_dex'])
 
 def clone(coords=(0,0,0)):
-    monster_types = {'orc': 10, 'goblin': 40, 'hobgoblin': 20, 'kobold': 30}
-    type_range_values = []
-    for p in list(monster_types):
-        for q in range(0, monster_types[p]):
-            type_range_values.append(p)
+    # probabilities should add up to 100
+    monster_types = [Monster_stats('orc','orcs',probability=10,av_ac=20,av_combat=40,av_strength=30,av_dex=40), 
+                     Monster_stats('goblin','goblins',probability=40,av_ac=10,av_combat=30,av_strength=20,av_dex=40),
+                     Monster_stats('hobgoblin','hobgoblins',probability=20,av_ac=30,av_combat=50,av_strength=40,av_dex=60),
+                     Monster_stats('kobold','kobolds',probability=30,av_ac=10,av_combat=20,av_strength=10,av_dex=40)]
+    rand = random.randrange(1,100)
+    for m in monster_types:
+        rand -= m.probability
+        if rand <= 0: 
+            break
 
-    monster_type = type_range_values[random.randint(0,99)]
-    monster = creature.NPC('%s' % monster_type, __file__, random.randint(1,2))
-    monster.set_description('%s' % monster_type, 'This is a %s.' % monster_type)
-    monster.set_combat_vars(random.randint(8, 16), random.randint(5, 30), random.randint(15,25), random.randint(5, 30))
-    # TODO: Base these attributes on monster type and coordinates
+    monster = creature.NPC(m.name, __file__, random.randint(1,2))
+    monster.set_description(m.name, 'This is a %s.' % m.name)
+    # TODO: vary these attributes by up to +/- 10% of the average
+    monster.set_combat_vars(m.av_ac, m.av_combat, m.av_strength, m.av_dex)
     return monster
