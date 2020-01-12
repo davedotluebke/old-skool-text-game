518f0770c8c9b415232b16f31826bb3b47dc285e
E3125 <edwin@luebke.us>
2020-01-12 05:34:38 -0500

    minor updates to cliff

--- domains/evergreen/mountaintop/cliff2.py
+++ domains/evergreen/mountaintop/cliff2.py
@@ -10,9 +10,13 @@ def load():
     
     cliff = room.Room('cliff', pref_id=roomPath)
     cliff.set_description('clifftop', 'You walk out onto a massive clifftop, overlooking an endless evergreen forest carpeted with snow. You see a small wood cottage with a small pine door.')
+    cliff.add_exit('east', 'domains.evergreen.mountaintop.cliff')
+    
     cottage = scenery.Scenery('cottage', 'small wood cottage', 'You see a small wood cottage, with a pine wood door.')
     cottage.move_to(cliff, True)
-    pine_door = StaffDoorway('door')
-    cliff.add_exit('east', 'domains.evergreen.mountaintop.cliff')
+
+    spruce_door = StaffDoorway('door','spruce door','you see a small, strong, spruce door,with a golden key hole.', 'home.tate.cottage', allowed_players_list=['tate'])
+    spruce_door.add_adjectives('spruce','strong','small')
+    spruce_door.move_to(cliff,True)
 
     return cliff
