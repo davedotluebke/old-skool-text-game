a889c0cf92b74918cd947c05203bdd1029accf0e
davedotluebke <dave@luebke.us>
2020-01-12 21:35:04 -0500

    minor code style change

--- domains/centrata/orc_quest/orc_hide.py
+++ domains/centrata/orc_quest/orc_hide.py
@@ -5,6 +5,6 @@ def clone():
     hide.set_description('hide armor', 'A long tunic made of tough, crudely tanned hide from some large '
         'animal.  Despite its crude construction, wearing it will some protection in a fight.')
     hide.add_adjectives('crude', 'long')
-    hide.add_names('hide','armor', 'tunic')
+    hide.add_names('hide', 'armor', 'tunic')
 
     return hide
