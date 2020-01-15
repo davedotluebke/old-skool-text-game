9e3a98d29098acd3aa160c247f8cf13d100e523e
davedotluebke <dave@luebke.us>
2020-01-14 20:44:56 -0500

    Added more orc

---
+++ domains/centrata/orc_quest/orc_studded.py
@@ -0,0 +1,10 @@
+import armor
+
+def clone():
+    studded = armor.Armor('armor', __file__, 25, 2)
+    studded.set_description('studded leather armor', 'A boiled leather tunic and leggings covered with metal '
+        'studs, this armor is heavy but effective.')
+    studded.add_adjectives('studded', 'leather')
+    studded.add_names('tunic', 'leggins')
+
+    return studded
