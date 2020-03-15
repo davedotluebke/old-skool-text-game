a84195d2301fc4cb05502f70576326de99ae11e8
Owen Luebke <owen@luebke.us>
2020-03-15 07:30:37 -0400

    Added `house` scenery item to lookout

--- domains/school/school/lookout.py
+++ domains/school/school/lookout.py
@@ -20,6 +20,11 @@ def load():
     thick_forest.add_adjectives('thick')
     thick_forest.move_to(lookout, True)
 
+    house = scenery.Scenery('house', 'little house', 
+        'This small house sits near a clearing in the forest. It seems to be in a slight state of disrepair.')
+    house.add_adjectives('little', 'small')
+    house.move_to(lookout, True)
+
     garden = scenery.Scenery('garden', 'school garden',
         'This school garden appears to have a large variety of plants growing in it. '
         'You can make out some truly large sunflowers.', unlisted=True)
