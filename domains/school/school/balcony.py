c686876defd534a14f86b72e60189d8988d1a08c
Owen Luebke <owen@luebke.us>
2020-03-14 20:43:04 -0400

    Added additional scenery objects to game Also fixed minor bugs

--- domains/school/school/balcony.py
+++ domains/school/school/balcony.py
@@ -12,4 +12,10 @@ def load():
     'and to your south you see a large forest. ')
     balcony.add_adjectives('small')
     balcony.add_exit('north', 'domains.school.school.arena')
+
+    waterfall = scenery.Scenery('waterfall', 'waterfall', 'This beautiful waterfall stands to the west of the balcony.', unlisted=True)
+    waterfall.move_to(balcony, True)
+
+    forest = scenery.Scenery('forest', 'large forest', 'This large forest continues the entire forseeable distance to the south.', unlisted=True)
+    forest.move_to(balcony, True)
     return balcony
