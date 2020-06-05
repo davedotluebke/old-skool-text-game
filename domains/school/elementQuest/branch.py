af03f347da68fd398bf335d9a77e5499d6cea5d2
1Bayshore <owen@luebke.us>
2020-06-05 07:00:59 -0400

    Created an improved fire_quest Created a game-wide fire system activated by changing the `flammable` attribute Changed firepit to be actual object, rather than enourmous mess Added several more rooms to the existing quest Added a match which can be used to light things on demand, but goes away once used Removed torn tapestry, liquid fire potion, cauldron, and wooden barricade Updated portal and fire lounge to new scroll system

--- domains/school/elementQuest/branch.py
+++ domains/school/elementQuest/branch.py
@@ -4,4 +4,7 @@ def clone():
     branch = thing.Thing('branch', __file__)
     branch.set_description('sturdy oak branch', 'This is a sturdy oak branch. It seems like it could burn for quite a while.')
     branch.add_adjectives('sturdy', 'oak')
+    branch.flammable = 7
+    branch.on_fire_description = 'This is a sturdy oak branch. It is currently on fire.'
+    branch.burn_time = 500
     return branch
--- domains/school/scroll.py
+++ domains/school/scroll.py
@@ -16,24 +16,32 @@ class Scroll(thing.Thing):
     #
     # Other miscelaneous methods
     #
+
+    def move_to(self, dest, force_move=False, merge_pluralities=True):
+        value = super().move_to(dest, force_move=force_move, merge_pluralities=merge_pluralities)
+        self.emit('The scroll glows for a second!')
+        return value
     
     #
     # Action methods and dictionary (methods come first)
     #
     def read(self, p, cons, oDO, oIDO):
-        if cons.user != self.location:
-            cons.user.perceive("The scroll is mysteriously blank.")
+        if self == oDO or self == oIDO:
+            if cons.user != self.location:
+                cons.user.perceive("The scroll is mysteriously blank.")
+                return True
+            
+            msg = "On this scroll you read:\n\n"
+            for i in self.location.quest_list:
+                if i[1] == True: # quest is complete
+                    msg += f"~~{i[0]}~~\n" # XXX strikethrough not working right now
+                else:
+                    msg += i[0] + '\n'
+            
+            cons.user.perceive(msg)
             return True
-        
-        msg = "On this scroll you read:\n\n"
-        for i in self.location.quest_list:
-            if i[1] == True: # quest is complete
-                msg += f"~{i[0]}~\n" # XXX strikethrough not working right now
-            else:
-                msg += i[0] + '\n'
-        
-        cons.user.perceive(msg)
-        return True
+        else:
+            return "Not sure what you are trying to look at!"
     
     actions = dict(thing.Thing.actions)
     actions['read'] = action.Action(read, True, False)
