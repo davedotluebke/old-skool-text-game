2f90aa85bc885288cdd4041ee23637c2cab6ab24
1Bayshore <owen@luebke.us>
2020-06-11 14:32:24 -0400

    Added more rooms to air quest Bedroom balcony now has birdfeeders Added living space and lookout

--- domains/school/elementQuest/bedroom_balcony.py
+++ domains/school/elementQuest/bedroom_balcony.py
@@ -16,6 +16,23 @@ def load():
     east_door.add_adjectives('glass', 'bedroom')
     r.insert(east_door)
 
-    # TODO: add birdfeeders here
+    feeder_one = scenery.Scenery('feeder', 'cylendrical bird feeder', 'This is a small cylendrical bird feeder with several perches.')
+    feeder_one.add_adjectives('bird', 'cylendrical', 'small')
+    feeder_one.add_response(['take', 'get', 'steal', 'remove', 'destroy', 'break'], 'You consider removing the birdfeeders, but decide the birds would not like it.')
+    r.insert(feeder_one)
+
+    feeder_two = scenery.Scenery('feeder', 'hopper style bird feeder', 'This is an enormous hopper style bird feeder.')
+    feeder_two.add_adjectives('bird', 'hopper', 'style', 'hopper-style', 'enormous')
+    feeder_two.add_response(['take', 'get', 'steal', 'remove', 'destroy', 'break'], 'You consider removing the birdfeeders, but decide the birds would not like it.')
+    r.insert(feeder_two)
+
+    feeder_three = scenery.Scenery('feeder', 'suet feeder', 'This is a small suet feeder.')
+    feeder_three.add_adjectives('bird', 'suet', 'small')
+    feeder_three.add_response(['take', 'get', 'steal', 'remove', 'destroy', 'break'], 'You consider removing the birdfeeders, but decide the birds would not like it.')
+    r.insert(feeder_three)
+
+    mountains = scenery.Scenery('mountains', 'mountains', 'These mountains are tall, steep, and covered in snow in some sections.', unlisted=True)
+    mountains.add_adjectives('tall', 'steep', 'snowy')
+    r.insert(mountains)
     
     return r
