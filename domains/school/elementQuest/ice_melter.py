91838364cf0f70ff8017e55b7b2db0792f51ca41
Edwin Luebke <edwin@Davids-MacBook-Pro.local>
2020-06-11 13:19:29 -0400

    worked on air quest

--- domains/school/elementQuest/ice_melter.py
+++ domains/school/elementQuest/ice_melter.py
@@ -7,3 +7,4 @@ def clone():
     ice_melter.fix_in_place('You try as hard as you can but you just can\'t budge the ice melter.')
     ice_melter.add_adjectives('hot', 'ice', 'small', 'normal')
     ice_melter.set_description('ice maker', 'This is a small but normal ice melter. It is hot.')
+    ice_melter.
