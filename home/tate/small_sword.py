638a5a680e972f5e674f34c4ead2fc55dfb29a1f
Old Skool Text Gitbot <gitbot@luebke.us>
2020-06-13 13:20:13 +0000

    tate uploaded file /home/tate/small_sword.py

--- home/tate/small_sword.py
+++ home/tate/small_sword.py
@@ -1,7 +1,7 @@
 import weapon
 
 def clone():
-    knife = weapon.Weapon('knife', __file__, 14, 17, 1.5)
-    knife.set_description('small sword', 'This is a small sword, made out of steel. The blade gleems in the sun.')
-    knife.add_adjectives('sharp')
-    return knife
+    sword = weapon.Weapon('knife', __file__, 14, 17, 1.5)
+    sword.set_description('small sword', 'This is a small sword, made out of steel. The blade gleems in the sun.')
+    sword.add_adjectives('sharp')
+    return sword
