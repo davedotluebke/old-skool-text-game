e5e99fd3fa697aabef0387aee6e39acf8706b204
Old Skool Text Gitbot <gitbot@luebke.us>
2020-06-13 11:58:05 +0000

    tate uploaded file /home/tate/strong_suit.py

---
+++ home/tate/strong_suit.py
@@ -0,0 +1,9 @@
+import armor
+
+def clone():
+    leather_suit = armor.Armor('leather_suit', __file__, 37, 1.5)
+    leather_suit.set_description('warm leather suit', 'This leather suit consists of: boots, pants, a tunic, and a cap, made of very sturdy leather. It looks like it provides a lot of warmth and protection.')
+    leather_suit.add_adjectives('leather', 'strong')
+    leather_suit.add_names(,'suit')
+
+    return leather_suit     
