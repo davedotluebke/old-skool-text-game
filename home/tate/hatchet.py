0a2c460406f7be4c4015830371468fb8d6d5073e
Old Skool Text Gitbot <gitbot@luebke.us>
2020-06-13 12:33:21 +0000

    tate uploaded file /home/tate/hatchet.py

--- home/tate/hatchet.py
+++ home/tate/hatchet.py
@@ -3,5 +3,6 @@ import weapon
 def clone():
     hatchet = weapon.Weapon('hatchet', __file__, 14, 16, 2)
     hatchet.set_description('small hatchet', 'This small hatchet has a nice mahogany handle, the rest is stainless steel.')
-    hatcheet.add_adjectives('small', 'powerful')
+    hatchet.add_adjectives('small', 'powerful')
+    
     return hatchet
