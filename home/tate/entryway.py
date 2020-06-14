b35dad70f5022bec4f74d6bc68e5a6334df0385c
Old Skool Text Gitbot <gitbot@luebke.us>
2020-06-14 14:37:33 +0000

    tate uploaded file /home/tate/entryway.py

--- home/tate/entryway.py
+++ home/tate/entryway.py
@@ -11,5 +11,5 @@ def load():
     sea_otter_hall.set_description('blue painted room', 'This room is painted blue from floor to ceiling. On the walls you notice a few carvings of sea otters.')
     sea_otter_hall.add_adjectives('blue', 'painted', 'otter')
     sea_otter_hall.add_exit('south', 'domains.school.school.hallway')
-    sea_otter_hall.add_exit('north', 'home.tate.palm')
+    sea_otter_hall.add_exit('north', 'home.tate.tatelivingspace')
     return sea_otter_hall
