f5bb4c5478495dd15c5fcfd3e7c64c5fe0f44550
Edwin Luebke <edwin@Davids-MacBook-Pro.local>
2020-06-16 14:50:26 -0400

    fixed bugs on bookcase

--- domains/school/school/library_bookcase.py
+++ domains/school/school/library_bookcase.py
@@ -18,10 +18,10 @@ class Bookcase(Container):
         self.books = [
             gametools.clone('domains.school.school.potion_book'),
             gametools.clone('domains.school.school.blue_book'),
-            gametools.clone('domains.school.school.secret_passage_book')
-            gametools.clone('domains.school.school.romeo and juliet')
+            gametools.clone('domains.school.school.secret_passage_book'),
+            gametools.clone('domains.school.school.romeo_and_juliet')
         ]
-        for i in range(0, len(self.books):
+        for i in range(0, len(self.books)):
             self.books.append(gametools.clone('domains.school.school.random_book'))
         
         for j in self.books:
