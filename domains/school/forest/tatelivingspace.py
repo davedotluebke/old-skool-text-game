8e1acd893d45a62653d9dd82a8d97455ef57a3e0
1Bayshore <owen@luebke.us>
2020-06-16 10:02:04 -0400

    Fixed tate living space

--- domains/school/forest/tatelivingspace.py
+++ domains/school/forest/tatelivingspace.py
@@ -13,9 +13,9 @@ def load():
     bedroom.add_exit('up', 'home.tate.bedroom')
     bedroom.add_exit('south', 'home.tate.entryway')
     coach = scenery.Scenery('coach','cozy nice coach', 'This cozy coach is very nice looking.')
-    bed.add_adjectives('cozy', 'small')
-    bed.add_response(['sit down'], 'you sit down, it is very comfy.', True, True)
-    bed.add_response(['get up'], 'you get up.', emit_message='%s looks around the room.')
+    coach.add_adjectives('cozy', 'small')
+    coach.add_response(['sit down'], 'you sit down, it is very comfy.', True, True)
+    coach.add_response(['get up'], 'you get up.', emit_message='%s looks around the room.')
     bedroom.insert(coach)
 
 
