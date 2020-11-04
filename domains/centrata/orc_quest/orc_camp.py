dc9a2efbd3446abaeaaedb42f89f76072cfda096
davedotluebke <dave@luebke.us>
2020-11-03 19:24:57 -0500

    Added descriptive text and scenery to orc camp. Could still use scenery responses, as well as scenery objects for the trash, heads, and firepit.

--- domains/centrata/orc_quest/orc_camp.py
+++ domains/centrata/orc_quest/orc_camp.py
@@ -9,7 +9,26 @@ def load():
     if exists: return exists
     
     orc_camp = room.Room('camp', roomPath)
-    orc_camp.set_description('orc camp', 'This is the orc camp, awaiting further description.')
+    orc_camp.set_description('orc camp', "This can only be the camp of the orcs. The tall grass of "
+        "the prairie here is trampled, scattered with trash, and smeared with unspeakable filth. A "
+        "line of closely-spaced stakes forms a makeshift barrier, a pile of rotting furs serves as a "
+        "crude bed, and a blackened firepit contains the charred remains of the orc's last  meal -- "
+        "charred bones, still smoking.")
+    furs = scenery.Scenery("furs", "pile of furs", "This pile of mangy furs and rotting hides is "
+        "thrown on the ground to serve as a bed. The smell of orc is almost overpowering.", unlisted=True)
+    furs.add_names("pile", "bed")
+    furs.add_adjectives("rotting", "mangy", "crude")
+    stakes = scenery.Scenery("stakes", "line of stakes", "A line of sharpened stakes have been driven "
+        "into the ground to form a crude but effective barrier. There is just enough room to slide "
+        "between the stakes, but they would certainly slow down attackers. Driven onto some of the "
+        "stakes are heads of unfortunate creatures, in varying stages of decomposition.", unlisted=True)
+    stakes.add_names("line", "barrier", "stake")
+    stakes.add_adjectives("crude", "sharpened", "line of", "closely-spaced", "makeshift")
+    bones = scenery.Scenery("bones", "charred bones", "These are the smoking bones of some large "
+        "creature.  You try not to think about just what (or who) it was.", unlisted=True)    
+    bones.add_names("bone")
+    bones.add_adjectives("charred")
+    
 
     if prairie.connection_exists(prairie.orc_camp_x-1, prairie.orc_camp_y, 1, 0, prairie.exit_probability):
         orc_camp.add_exit('west', f'domains.centrata.orc_quest.prairie?{prairie.orc_camp_x-1}&{prairie.orc_camp_y}')
