01767c5d8337af287ed6fb346b7da3a88ac6d370
1Bayshore <owen@luebke.us>
2021-11-08 06:25:03 -0500

    Removed deletions of magic and quest tracking from player.py in accordance with #186

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ import sys
 import importlib
 import connections_websock
 import os
+import magic
 import logging
 
 import gametools
@@ -91,6 +92,21 @@ class Player(Creature):
         self.game.register_heartbeat(self)
         self.versions[gametools.findGamePath(__file__)] = 3
         self.prev_location_id = None
+        # Convention: Each spell is a key-value pair where the key is the name
+        # of the spell (for the player, e.g. 'apparate') and the value is a 
+        # python-style path to the spell (e.g. 'spells.apparate')
+        self.spellsKnown = {}
+        self.max_mana = 100
+        self.mana = self.max_mana
+        self.terse = False  # True -> show short description when entering room
+        self.game.register_heartbeat(self)
+        self.versions[gametools.findGamePath(__file__)] = 2
+        self.prev_location_id = None
+        self.quest_list = [ # list of quests this player knows about, followed by completion status (True or False)
+            ["Find the library", False],
+            ["Create a potion to hide thyself and use it to sneak past a terrible monster", False],
+            ["Complete a quest to find thy element", False]
+        ]
         self.tutorial_messages = {
             'domains.character_creation.species': 'Try typing "look north mirror" and then "enter north mirror".',
             'domains.character_creation.adjective1': 'Try intoning something from the plaque.',
@@ -257,6 +273,11 @@ class Player(Creature):
         if rid_act in self.tutorial_act_messages:
             self.cons.write(self.tutorial_act_messages[rid_act])
             self.tutorial_act_messages_complete[rid_act] = True
+    
+    def restore_mana(self):
+        if self.mana < self.max_mana:
+            self.mana += 1
+
     #
     # SET/GET METHODS (methods to set or query attributes)
     #
@@ -271,6 +292,66 @@ class Player(Creature):
             pass
         del saveable['cons']
         return saveable
+    
+    def get_mana(self):
+        return self.mana
+    
+    def calc_chance_mess_up(self, mana):
+        if mana == 0:
+            return 0
+        chanceWrong = (pow(10, 4*((mana-self.max_mana)/mana))- 1) / 100
+        if chanceWrong < 0:
+            return 0
+        elif chanceWrong > 1:
+            return 1
+        else:
+            return chanceWrong
+        
+    def get_max_mana(self):
+        return self.max_mana
+
+    def set_mana(self, mana):
+        try:
+            intmana = int(mana)
+            if intmana != mana:
+                self.log.warning('Converted mana to integer')
+            if mana >= 0:
+                self.mana = intmana
+            else:
+                self.log.error('Mana must be non-negative')
+        except TypeError:
+            self.log.error('Mana must be a non-negative integer')
+
+    def set_max_mana(self, mana):
+        try:
+            intmana = int(mana)
+            if intmana != mana:
+                self.log.warning('Converted max_mana to integer')
+            if mana >= 0:
+                self.mana = intmana
+            else:
+                self.log.error('Mana must be non-negative')
+        except TypeError:
+            self.log.error('Mana must be a non-negative integer')
+    
+    def add_quest(self, quest_message):
+        """Add a quest to the player's list of quests."""
+        self.quest_list.append([quest_message, False])
+    
+    def complete_quest(self, quest_message):
+        """Complete a quest, setting its "complete" flag to True."""
+        l = [self.quest_list.index(x) for x in self.quest_list if x[0] == quest_message]
+        for i in l:
+            self.quest_list[i][1] = True
+    
+    def remove_quest(self, quest_message):
+        """Remove a quest from a player's list of quests. 
+        This is equivalent to cancelling the quest. To
+        mark a quest as complete, use the "complete quest"
+        method instead."""
+        l = [self.quest_list.index(x) for x in self.quest_list if x[0] == quest_message]
+        for i in l:
+            del self.quest_list[i]
 
     #
     # OTHER EXTERNAL METHODS (misc externally visible methods)
@@ -288,6 +369,9 @@ class Player(Creature):
         
         if self.health < self.hitpoints:
             self.heal()
+        
+        if self.mana < self.max_mana:
+            self.restore_mana()
         
         cmd = self.cons.take_input()
         if self.login_state != None:
@@ -925,5 +1009,7 @@ class Player(Creature):
     actions['engage'] =     Action(engage, True, False)
     actions['attack'] =     Action(engage, True, False)
     actions['disengage'] =  Action(disengage, False, True)
+    actions['cast'] =       Action(magic.castChecks, True, False)
     actions['give'] =       Action(give, True, False)
     actions['offer'] =      Action(give, True, False)
+
