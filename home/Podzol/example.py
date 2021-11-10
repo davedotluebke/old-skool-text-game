e02ef227d36487e6d0e5eb502544aabc0d7e2aed
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-10 22:35:44 +0000

    Updated player roles

---
+++ home/Podzol/example.py
@@ -0,0 +1,7 @@
+import thing
+
+def clone():
+    example = thing.Thing('example', __file__)
+    example.set_description('example object', 'This is an example object for testing purposes.')
+    example.add_adjectives('example')
+    return example
