98e1e8ec7b9aad51d1d25ca1e6b866a1c489bae1
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-12 17:21:37 +0000

    Podzol uploaded file /home/Podzol/ring.py

---
+++ home/Podzol/ring.py
@@ -0,0 +1,7 @@
+import thing
+
+def clone():
+    ring = thing.Thing('ring', __file__)
+    ring.set_description('example object', 'This is an example object for testing purposes.')
+    ring.add_adjectives('example')
+    return example
