ac41ebe14dd89ba8a9eb273040ebcb80eba481a4
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-15 00:56:21 +0000

    Corrected time delay on run_once to account for game starting at 5:00 UTC rather than 0:00 UTC

--- run_once.sh
+++ run_once.sh
@@ -3,7 +3,7 @@
 cd /game
 # magic command for finding public IP of current instance	
 myAddress="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"  	
-timeToReset="$((86378-$(date -d "1970-01-01 UTC $(date +%T)" +%s)))" # run until 22 seconds before the day ends
+timeToReset="$((104378-$(date -d "1970-01-01 UTC $(date +%T)" +%s)))" # run until 22 seconds before the day ends
 python3 startup.py --server "$myAddress" --mode "nocrypt" --duration "$timeToReset"
 chgrp -R wizards saved_players
 chmod -R ug+rw saved_players
