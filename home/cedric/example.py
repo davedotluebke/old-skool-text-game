780444c228c151fb784d2ab603ff7d7bc8b31330
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-15 01:11:51 +0000

    Continuing to test gameserver gitbot

--- home/cedric/example.py
+++ home/cedric/example.py
@@ -3,6 +3,6 @@ import gametools
 
 def clone():
     example = thing.Thing('example', __file__)
-    example.set_description('example object', 'This is an example object for testing purposes.')
+    example.set_description('example object', 'This is a new (Nov 14, 2021) example object for testing purposes.')
     example.add_adjectives('example', 'notional')
     return example
