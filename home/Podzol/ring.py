0828a1e54bae68e65d730b35055bdac969a601f1
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-19 23:38:58 +0000

    Podzol uploaded file /home/Podzol/ring.py

--- home/Podzol/ring.py
+++ home/Podzol/ring.py
@@ -2,6 +2,6 @@ import thing
 
 def clone():
     ring = thing.Thing('ring', __file__)
-    ring.set_description('example object', 'This is an example object for testing purposes.')
-    ring.add_adjectives('example')
-    return example
+    ring.set_description('brass ring', "A small brass ring, masterfully engraved with celtic knots. The knots wrap around a unicorn's head at the front.")
+    ring.add_adjectives('brass','unicorn','celtic','metal','small')
+    return ring
