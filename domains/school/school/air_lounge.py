8dc3290cd34bf840130645deb96bcf37a76f3571
Old Skool Text Gitbot <gitbot@luebke.us>
2021-11-21 16:47:50 +0000

    scott uploaded file /domains/school/school/air_lounge.py

--- domains/school/school/air_lounge.py
+++ domains/school/school/air_lounge.py
@@ -7,4 +7,6 @@ def load():
     if exists: return exists
     
     air_lounge = room.Room('lounge', roomPath, safe=True)
-    air_lounge.set_description()
+    air_lounge.set_description('high tower', 'This high tower feels as if it is nearly touching the sky. A black metal rail surrounds the tower, except to the south where a rainbow shines down on a stone bridge. The ground is covered in soft white pillows. There is an oak trapdoor in the centre of the tower.')
+    air_lounge.add_exit('south', 'domains.school.school.air_bridge')
+    return air_lounge
