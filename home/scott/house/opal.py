49443db111840e35e356eaf90181273f9b56e126
Old Skool Text Gitbot <gitbot@luebke.us>
2022-12-26 17:40:57 +0000

    scott uploaded file /home/scott/house/opal.py

--- home/scott/house/opal.py
+++ home/scott/house/opal.py
@@ -1,5 +1,5 @@
 import domains.wizardry.gems as gems
 
 def clone():
-    opal = gems.Opal(__file__, 'opal', 'jagged-cut opal', 'This opal is jaggedly cut.')
+    opal = gems.Opal(__file__, 'opal', 'jagged-cut opal', 'This opal is very jaggedly cut.')
     return opal
