d63a0bba00a4420bb0b65962b9c66fcde5d08273
1Bayshore <owen@luebke.us>
2022-12-27 13:37:51 -0500

    Added a basic implementation of openAI for random books

--- domains/school/school/random_book.py
+++ domains/school/school/random_book.py
@@ -1,5 +1,26 @@
 import random
 import domains.school.school.library_book as library_book
+try:
+    import openai
+    openai.api_key = "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"
+    ai_installed = True
+except ImportError:
+    ai_installed = False
+
+def openai_completion(prompt):
+    if not ai_installed:
+        raise ImportError("openAI was not installed")
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1000,
+            temperature=0.5
+        )
+    except Exception as e:
+        print(e)
+        return ""
+    return response['choices'][0]['text']
 
 title_formats = ['The {adjective1} {noun1}', '{noun1}, {noun2}, and the {adjective1} {noun3}', 
     'How the {noun1} {verb1}èd the {noun2}', 'A tale of {number1} {noun1}s', '{noun1} and {noun2}',
@@ -17,6 +38,10 @@ book_description_adjectives = ['old', 'worn', 'dusty', 'new', 'shiny', 'red', 'o
 book_messages = ['This page is in a language that you do not understand.', 'This page is mysteriously blank.', 
     'This page is too worn for you to make out the text.', 'This page has been blotted out by an ink spill.']
 book_message_weights = [45, 8, 40, 7]
+
+book_styles = ["prose", "poetry"]
+book_style_weights = [50, 50]
+styling = False
 
 def clone():
     t_format = random.choice(title_formats)
@@ -41,18 +66,29 @@ def clone():
     book_s_desc = '%s %s book' % tuple(book_adjectives)
     book_l_desc = 'This is a ' + book_s_desc + ' titled ' + book_title + '.'
 
-    number_pages = round(random.normalvariate(30, 15))
+    if not ai_installed:
+        number_pages = round(random.normalvariate(30, 15))
+        page_msg = random.choices(book_messages, book_message_weights)[0]
 
-    page_msg = random.choices(book_messages, book_message_weights)[0]
     book_msg = """
 \=============================================
 %s
 \=============================================""" % book_title
    
-    for i in range(0, number_pages):
-        book_msg += "\n#*\n" + page_msg
+    if not ai_installed:
+        for i in range(0, number_pages):
+            book_msg += "\n#*\n" + page_msg
+    else:
+        if styling:
+            book_style = random.choices(book_styles, book_style_weights)[0]
+            ai_body = openai_completion(("write a book titled %s in the style of %s" % (book_title, book_style)))
+        else:
+            ai_body = openai_completion(("write a book titled %s" % book_title))
+        book_msg += "\n#*\n#*\n" + ai_body
     
     random_book = library_book.LibraryBook('book', __file__, book_s_desc, book_l_desc)
     random_book.add_adjectives(*book_adjectives)
     random_book.set_message(book_msg)
     return random_book
+
+#print(openai_completion("write a book titled Dragons and Coffee: A Retrospective"))
