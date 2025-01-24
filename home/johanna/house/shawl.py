a7b032f03c1bed63289966608f2b8b41b06c0d21
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-24 00:40:40 +0000

    johanna uploaded file /home/johanna/house/shawl.py

--- home/johanna/house/shawl.py
+++ home/johanna/house/shawl.py
@@ -1,7 +1,7 @@
 import thing
 
 def clone():
-    shawl = Thing('shawl', __file__)
+    shawl = thing.Thing('shawl', __file__)
     shawl.set_description('bright orange shawl', 'This bright orange shawl is very clean and soft.')
     shawl.set_weight(200)
     shawl.set_volume(0.1)
