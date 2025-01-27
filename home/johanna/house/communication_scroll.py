4964b0b3bf44ea641100ca5e3acbec8d0663a8a0
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-27 04:42:12 +0000

    johanna uploaded file /home/johanna/house/communication_scroll.py

--- home/johanna/house/communication_scroll.py
+++ home/johanna/house/communication_scroll.py
@@ -9,12 +9,15 @@ class CommunicationScroll(Thing):
     # SPECIAL METHODS (i.e __method__() format)
     #
     def __init__(self, pref_id=None, other_scroll_id=None):
+        self.log.debug('In the __init__ function of communication_scroll!')
         super().__init__('scroll', __file__, pref_id=pref_id)
         self.written_on = 'This scroll is magical. Try writing on it.'
         self.set_description('tattered scroll', 'This scroll is tattered, but you can still make out the following: ' + self.written_on)
         self.add_adjectives('tattered')
         
         self.other_scroll_id = other_scroll_id
+        
+		self.log.debug('Finished the __init__ function of communication_scroll!')
     
     #
     # GET/SET METHODS
@@ -54,19 +57,27 @@ class CommunicationScroll(Thing):
 #
 def clone(params):
     if len(params) == 2:
-    	matching_scroll_id=params[0], matching_location_path=params[1]
+    	matching_scroll_id=params[0]
+        matching_location_path=params[1]
+    else:
+        matching_scroll_id = None
+ 		matching_location_path = None
     
     scroll_id_number = f'communication_scroll{random.randint(0,100)}'
     scroll = CommunicationScroll(pref_id=scroll_id_number)
     
     if matching_scroll_id:
         scroll.other_scroll_id = matching_scroll_id
+        scroll.debug('communication_scroll added other_scroll_id!')
     
-    elif matching_location_path:        
+    elif matching_location_path:
+        scroll.debug('Decided to follow location path!')
         matching_location = gametools.load_room(matching_location_path)
+        scroll.debug('Loaded location!')
         for i in matching_location.contents:
             if isinstance(i, CommunicationScroll):
                 scroll.other_scroll_id = i.id
                 i.other_scroll_id = scroll.id
+                scroll.debug('Found and updated matching scroll!')
 
     return scroll
