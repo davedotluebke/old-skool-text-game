6553711e4ca34a51e793eb42dce5210a898e6f36
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-27 21:07:02 +0000

    johanna uploaded file /domains/centrata/mansion/parlour.py

--- domains/centrata/mansion/parlour.py
+++ domains/centrata/mansion/parlour.py
@@ -11,6 +11,16 @@ def load():
     r.set_description('well-furnished parlour', 'You step into a well-furnished parlour, complete with several sofas and armchairs. The wallpaper is a dark red, pairing with the green of the furniture. Passages lead south and northwest, while there are doors to the north and the southwest.')
     r.add_exit('south', 'domains.centrata.mansion.office')
 
-    # TODO: Add more scenery
+    large_sofa = scenery.Scenery('sofa', 'large green sofa', 'This large dark green sofa is covered in a soft felt material.', unlisted=True)
+    large_sofa.add_adjectives('large', 'green', 'soft', 'felt')
+    large_sofa.add_response(['sit', 'lie', 'relax'], 'You relax on the sofa. The felt covering feels soft.')
+    large_sofa.add_response(['move', 'take', 'get'], 'The large sofa is far too heavy to move.')
+    r.insert(large_sofa, True)
+    
+    small_sofa = scenery.Scenery('sofa', 'small green sofa', 'This small dark green sofa is covered in a soft felt material.', unlisted=True)
+    small_sofa.add_adjectives('small', 'dark', 'green', 'soft', 'felt')
+    small_sofa.add_response(['sit', 'lie', 'relax'], 'You relax on the sofa. The felt covering feels soft.')
+    small_sofa.add_response(['move', 'take', 'get'], 'Despite being the smaller of the sofas, the small green sofa is still far too heavy to move.')
+    r.insert(small_sofa, True)
 
     return r
