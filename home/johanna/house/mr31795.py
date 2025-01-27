1e1ff1bb8413f0428b04b2ccc28f17d717eba736
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-27 05:47:29 +0000

    johanna uploaded file /home/johanna/house/mr31795.py

--- home/johanna/house/mr31795.py
+++ home/johanna/house/mr31795.py
@@ -36,7 +36,7 @@ def load():
     cauldron = gametools.clone('domains.school.school.cauldron')
     magic_room.insert(cauldron, True)
     
-    comm_scroll = gametools.clone('home.johanna.house.communication_scroll0', params=[None, 'home.johanna.house.br31795'])
+    comm_scroll = gametools.clone('home.johanna.house.communication_scroll')
     magic_room.insert(comm_scroll, True)
     
     return magic_room
