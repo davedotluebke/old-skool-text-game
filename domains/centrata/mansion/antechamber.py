9a95722d7e43a9f6a1c21eca484ee856fb671bc3
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-28 20:23:54 +0000

    johanna uploaded file /domains/centrata/mansion/antechamber.py

--- domains/centrata/mansion/antechamber.py
+++ domains/centrata/mansion/antechamber.py
@@ -19,26 +19,4 @@ def load():
     front_door.add_adjectives('large', 'grandiose', 'front')
     front_door.locked = True
 
-    large_sofa = scenery.Scenery('sofa', 'large green sofa', 'This large dark green sofa is covered in a soft felt material.', unlisted=True)
-    large_sofa.add_adjectives('large', 'green', 'soft', 'felt')
-    large_sofa.add_response(['sit', 'lie', 'relax'], 'You relax on the sofa. The felt covering feels soft.')
-    large_sofa.add_response(['move', 'take', 'get'], 'The large sofa is far too heavy to move.')
-    r.insert(large_sofa, True)
-    
-    small_sofa = scenery.Scenery('sofa', 'small green sofa', 'This small dark green sofa is covered in a soft felt material.', unlisted=True)
-    small_sofa.add_adjectives('small', 'dark', 'green', 'soft', 'felt')
-    small_sofa.add_response(['sit', 'lie', 'relax'], 'You relax on the sofa. The felt covering feels soft.')
-    small_sofa.add_response(['move', 'take', 'get'], 'Despite being the smaller of the sofas, the small green sofa is still far too heavy to move.')
-    r.insert(small_sofa, True)
-    
-    armchair = scenery.Scenery('armchair', 'large green armchair', 'This large dark green armchair is covered in a soft felt material.', unlisted=True)
-    armchair.add_adjectives('large', 'green', 'soft', 'felt')
-    armchair.add_response(['sit', 'lie', 'relax'], 'You relax in the armchair. The felt covering feels soft.')
-    armchair.add_response(['move', 'take', 'get'], 'Despite being smaller than the two sofas, the armchair is far too heavy to move.')
-    r.insert(armchair, True)
-    
-    wallpaper = scenery.Scenery('wallpaper', 'red pattered wallpaper', 'This red pattered wallpaper is a dark red, with a slightly brighter red patterning showing long serpents.', unlisted=True)
-    wallpaper.add_adjectives('red', 'pattered', 'serpent')
-    r.insert(wallpaper, True)
-
     return r
