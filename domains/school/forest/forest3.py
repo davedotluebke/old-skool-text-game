4a880ec49947e6a6c3591a64e49f185d3f50d95d
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-28 21:56:20 +0000

    johanna uploaded file /domains/school/forest/forest3.py

--- domains/school/forest/forest3.py
+++ domains/school/forest/forest3.py
@@ -32,6 +32,4 @@ def load():
     truffles = gametools.clone('domains.school.forest.truffles')
     forest_three.insert(truffles)
     
-    flowerMod.ID_dict['amanda'].cons.write('Injection successful!')
-
     return forest_three
