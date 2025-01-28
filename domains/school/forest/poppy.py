15a32f52bbf1552bb71a87e6a8ba8acd9b2f0b11
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-28 21:55:53 +0000

    johanna uploaded file /domains/school/forest/poppy.py

--- domains/school/forest/poppy.py
+++ domains/school/forest/poppy.py
@@ -9,5 +9,4 @@ def clone():
     poppy.add_adjectives("very","pretty")
     poppy.actions = dict(flowerMod.Flower.actions)
     poppy.actions['pick'] = action.Action(flowerMod.Flower.take, True, False)
-    flowerMod.ID_dict['amanda'].cons.write('Injection successful!')
     return poppy
