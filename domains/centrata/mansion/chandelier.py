09d83bb52954f2099850505d15343d5dd86608b9
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-29 20:19:50 +0000

    johanna uploaded file /domains/centrata/mansion/chandelier.py

--- domains/centrata/mansion/chandelier.py
+++ domains/centrata/mansion/chandelier.py
@@ -2,7 +2,7 @@ import container
 
 def clone():
     chandelier = container.Container('chandelier', __file__)
-    chandelier.set_description('rose-tinted chandelier', 'This low-hanging chandelier is shimmering in brilliant rose colours that reflect throughout the entire room.')
+    chandelier.set_description('rose-tinted chandelier', 'This low-hanging chandelier is shimmering in brilliant rose colours that reflect throughout the entire room.', unlisted=True)
     chandelier.closable = False
     chandelier.add_adjectives('rose', 'tinted', 'rose-tinted', 'low', 'hanging', 'low-hanging', 'shimmering')
     chandelier.set_prepositions('on', 'onto')
