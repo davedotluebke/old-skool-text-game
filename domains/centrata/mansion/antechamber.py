4a4b03809ffc9d70b60436d409b73eb665b64cf3
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-29 23:29:00 +0000

    johanna uploaded file /domains/centrata/mansion/antechamber.py

--- domains/centrata/mansion/antechamber.py
+++ domains/centrata/mansion/antechamber.py
@@ -21,7 +21,7 @@ def load():
     front_door.locked = True
     r.insert(front_door, True)
     
-    chandelier = gametools.clone('domains.centrata.mansion.chandelier')
+    chandelier = gametools.clone('domains.centrata.mansion.chandelier1')
     r.insert(chandelier, True)
 
     return r
