33afe85e7112e9a8fddcafbef3bf42130e9d10e1
Old Skool Text Gitbot <gitbot@luebke.us>
2025-01-29 20:34:39 +0000

    johanna uploaded file /domains/centrata/mansion/chandelier.py

--- domains/centrata/mansion/chandelier.py
+++ domains/centrata/mansion/chandelier.py
@@ -1,10 +1,19 @@
 import container
 
+class Chandelier(container.Container):
+    def insert(self, obj, force_insert=False, merge_pluralities=True):
+        result = super().insert(obj, force_insert, merge_pluralities)
+        if result: # reminder, insert() returns True if insert failed
+            # chandelier comes crashing down
+            pass
+        return result
+
 def clone():
-    chandelier = container.Container('chandelier', __file__)
+    chandelier = Chandelier('chandelier', __file__)
     chandelier.set_description('rose-tinted chandelier', 'This low-hanging chandelier is shimmering in brilliant rose colours that reflect throughout the entire room.', unlisted=True)
     chandelier.closable = False
     chandelier.add_adjectives('rose', 'tinted', 'rose-tinted', 'low', 'hanging', 'low-hanging', 'shimmering')
     chandelier.set_prepositions('on', 'onto')
+    chandelier.fix_in_place('The chandelier is attached to the ceiling')
     
     return chandelier
