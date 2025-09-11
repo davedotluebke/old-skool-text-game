6aa75cac03311ef676c0bf73f9cd60ec539da8e0
davedotluebke <dave@luebke.us>
2025-09-11 12:26:36 -0700

    Added test_quest method to Player class and updated Innkeeper logic to check quest completion before offering rewards.

--- domains/centrata/village/innkeeper.py
+++ domains/centrata/village/innkeeper.py
@@ -13,12 +13,26 @@ class Innkeeper(shop.Shopkeeper):
                 if isinstance(i, player.Player):
                     for j in i.contents:
                         if hasattr(j, 'orc_quest') and j.orc_quest:
-                            self.orc_quest_completed = True
-                            self.scripts = []
 
-                            # offer the reward
+                            # offer the reward if they haven't already completed the quest
+                            if not i.test_quest("Bring the orc chief's helmet to the innkeeper"):
+                                self.orc_quest_completed = True
+                                self.say("""Congratulations again on your victory! The whole village is grateful to you.""")
+                                self.say("""Here's your reward, adventurer.  I'm sure you'll find a use for it.""")
+                                reward = gametools.clone('domains.centrata.village.gold_piece')
+                                reward.plurality = 10
+                                reward.move_to(i)
+                                i.complete_quest("Bring the orc chief's helmet to the innkeeper")
+                                j.orc_quest = False  # this helmet object has now been used to satisfy the quest
+                                # add the new quest-completed scripts
+                                self.scripts = []
+                                self.add_script("""Now the orcs are taken care of, the village will prosper.""")
+                                self.add_script("""Our tourist industry is sure to take off. Tourists like prairies, right?""")
+                                break
+                    else:
+                        if not i.test_quest("Bring the orc chief's helmet to the innkeeper"):
+                            i.add_quest("Bring the orc chief's helmet to the innkeeper")
 
-                            # add the new quest completed scripts
         return super().heartbeat()
 
 def clone():
